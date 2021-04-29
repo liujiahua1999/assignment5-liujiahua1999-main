e866dd7a59a6580907b77689a9ef4ef106981adb
Jiahua Liu <jxl6283@psu.edu>
2021-04-29 09:28:40 +0000

    .

--- net.c
+++ net.c
@@ -17,224 +17,124 @@ int cli_sd = -1;
 /* attempts to read n bytes from fd; returns true on success and false on
  * failure */
 static bool nread(int fd, int len, uint8_t *buf) {
-int num_read = 0;
-while (num_read < len)
-{
-  int n = read(fd,&buf[num_read], len - num_read);
-  if (n < 0)
-  {
-    return false;
-  }
-  if (n = 0)
-  {
-    return true;
-  }
-  if (n > 0)
-  {
-    num_read += n;
+  if (recv(cli_sd, buf, len, 0) < 0) {
 
-  } 
-}
-return true;
+		return false;
+	}
+  return true;
 }
 
 /* attempts to write n bytes to fd; returns true on success and false on
  * failure */
 static bool nwrite(int fd, int len, uint8_t *buf) {
-int num_write = 0;
-while (num_write < len)
-{
-  int n = write(fd,&buf[num_write], len - num_write);
-  if (n < 0)
-  {
-    return false;
-  }
-  if (n = 0)
-  {
-    return true;
-  }
-  if (n > 0)
-  {
-    num_write += n;
+  if (send(fd, buf, len, 0) < 0) {
 
-  }
-  
-}
-return true;
+		return false;
+	}
+  return true;
 }
 
 /* attempts to receive a packet from fd; returns true on success and false on
  * failure */
 static bool recv_packet(int fd, uint32_t *op, uint16_t *ret, uint8_t *block) {
-  
-  uint16_t len;
-  uint8_t header[HEADER_LEN];
-
-  if (!nread(fd,HEADER_LEN,header))
-  {
-    return false;
-
-  }
-
-
-  int offset = 0;
-  memcpy(&len, header + offset, sizeof(len));
-  offset += sizeof(len);
-  memcpy(op, header + offset, sizeof(*op));
-  offset += sizeof(*op);
-  memcpy(ret, header + offset, sizeof(*ret));
-    len = ntohs(len);
-  *op = ntohl(*op);
-  *ret = ntohs(*ret);
-
-
-return true;
 }
 
 /* attempts to send a packet to sd; returns true on success and false on
  * failure */
 static bool send_packet(int sd, uint32_t op, uint8_t *block) {
-  uint32_t cmd = op >> 26;
-  if(cmd == JBOD_WRITE_BLOCK)
-  {
-        uint16_t len = 8;
-  uint8_t header[HEADER_LEN];
 
-
-  len = ntohs(len);
-  op = ntohl(op);
-
-  int offset = 0;
-  memcpy(header + offset,&len, sizeof(len));
-  offset += sizeof(len);
-  memcpy(header + offset, op, sizeof(op));
-
-  if (!nwrite(sd,HEADER_LEN,header))
-  {
-    return false;
-
-  }
-
-  return true;
-  }
-  
-  if(cmd == JBOD_READ_BLOCK)
-  {
-        uint16_t len = 264;
-  uint8_t header[HEADER_LEN];
-
-
-  len = ntohs(len);
-  op = ntohl(op);
-
-  int offset = 0;
-  memcpy(header + offset,&len, sizeof(len));
-  offset += sizeof(len);
-  memcpy(header + offset, op, sizeof(op));
-  offset += sizeof(op);
-  memcpy(header + offset, *block, sizeof(*block));
-  if (!nwrite(sd,HEADER_LEN,header))
-  {
-    return false;
-
-  }
-  return true;
-  }
-return true;
 }
 
 /* attempts to connect to server and set the global cli_sd variable to the
  * socket; returns true if successful and false if not. */
 bool jbod_connect(const char *ip, uint16_t port) {
-  //Create socket
-  
-  cli_sd =socket(PF_INET, SOCK_STREAM, 0);
-  if (cli_sd == -1)
-  {
-    return false;
-  }
+  struct sockaddr_in server;
+  fprintf(stderr, "connected to the JBOD server at 127.0.0.1\n");
 
-  //convert ip to binary form
-  struct sockaddr_in caddr;
-  caddr.sin_family = AF_INET;
-  caddr.sin_port = htons(port);
-  if (inet_aton(ip,&caddr.sin_addr) == 0)
-  {
-    return false;
-  }
+  cli_sd = socket(AF_INET, SOCK_STREAM, 0);
+	if (-1 == cli_sd) {
 
-  
-  //connect
-  if (connect(cli_sd,(const struct sockaddr *)&caddr,sizeof(caddr)) == -1)
-  {
-    return false;
-  }
-  return true;
+    return -1;
+	}
+
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_family = AF_INET;
+	server.sin_port = htons(3333);
+
+	if (connect(cli_sd, (struct sockaddr *)&server, sizeof(server)) < 0) {
+
+		return -1;
+	}
+  return 1;
 }
-
 
 /* disconnects from the server and resets cli_sd */
 void jbod_disconnect(void) {
-  //close connection
   close(cli_sd);
   cli_sd = -1;
-
 }
 
 /* sends the JBOD operation to the server and receives and processes the
  * response. */
-
 int jbod_client_operation(uint32_t op, uint8_t *block) {
-  //loop 
-//op =
+
+
   uint32_t cmd = op >> 26;
-  if(cmd == JBOD_READ_BLOCK)
+  uint32_t disk_num = op & 0x3FFFFFF >> 22;
+  uint32_t block_num = (op & 0xFF);
+
+  //printf("jbod_client_operation,op:%x, cmd:%d, disk_num:%d, block_num:%d\n", op, cmd, disk_num, block_num);
+
+  char buf[264] = {0};
+  char buf2[264] = {0};
+
+
+  buf[2] = (op >> 24) & 0xFF;
+  buf[3] = (op >> 16) & 0xFF;
+  buf[4] = (op >> 8) & 0xFF;
+  buf[5] = op & 0xFF;
+  uint32_t len = 8;
+
+
+  if(cmd == JBOD_WRITE_BLOCK)
   {
-  uint16_t len = 264;
-  uint8_t header[HEADER_LEN];
-
-
-  len = ntohs(len);
-  op = ntohl(op);
-
-  int offset = 0;
-  memcpy(header + offset,&len, sizeof(len));
-  offset += sizeof(len);
-  memcpy(header + offset, op, sizeof(op));
-  offset += sizeof(op);
-  memcpy(header + offset, block, sizeof(block));
-    if (!nwrite(cli_sd,HEADER_LEN,header))
-    {
-      return false;
-    }
-  return true;
+	 memcpy(buf+8, block, 256);
+	 len = 264;
   }
 
-  else
+  buf[0] = htons(len) & 0xFF;
+  buf[1] = (htons(len) >> 8) & 0xFF;
+
+  if(true != nwrite(cli_sd, len, buf))
   {
-  uint16_t len = 8;
-  uint8_t header[HEADER_LEN];
-
-
-  len = ntohs(len);
-  op = ntohl(op);
-
-  
-  memcpy(header,len, 2);
-  
-  memcpy(header + 2, op, 4);
-
-  if (!nwrite(cli_sd,HEADER_LEN,header))
-  {return false;}
-
-  return true;
+	  printf("net write nok\n");
   }
-  
+  len = 8;
 
-return true;
+  if(cmd == JBOD_READ_BLOCK || cmd == JBOD_SIGN_BLOCK)
+  {
+	  len = 264;
+  }
 
-    //if packet size does include block (is long)
+  buf2[0] = htons(len) & 0xFF;
+  buf2[1] = (htons(len) >> 8) & 0xFF;
+  if(true != nread(cli_sd, len, buf2))
+  {
+	  printf("net read nok\n");
+  }
 
-    //is this the right way to write the code?
+  if(cmd == JBOD_READ_BLOCK || cmd == JBOD_SIGN_BLOCK)
+  {
+	  //printf("%s\n", buf2);
+	  memcpy(block, buf2+8, 256);
+  }
+
+
+   return true;
+
+
+
+
+
 
 }
